c6e10aea01ba0bb38070e1b114ec3712430dd762
mclpio <113801014+MclPio@users.noreply.github.com>
2026-03-30 13:08:06 -0400

    Fix download path

--- validate/download_amazon_esci.py
+++ validate/download_amazon_esci.py
@@ -8,11 +8,11 @@ from urllib.request import urlretrieve
 
 DEFAULT_FILES = {
     "shopping_queries_dataset_examples.parquet": (
-        "https://github.com/amazon-science/esci-data/raw/main/"
+        "https://github.com/amazon-science/esci-data/raw/main/shopping_queries_dataset/"
         "shopping_queries_dataset_examples.parquet"
     ),
     "shopping_queries_dataset_products.parquet": (
-        "https://github.com/amazon-science/esci-data/raw/main/"
+        "https://github.com/amazon-science/esci-data/raw/main/shopping_queries_dataset/"
         "shopping_queries_dataset_products.parquet"
     ),
 }
